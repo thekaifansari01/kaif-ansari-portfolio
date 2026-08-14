4e6c885b4c1fbe28d0d1b2a33f99c3128eb681b7
Kaif Ansari <kaif.ansari.global@gmail.com>
2026-08-14 09:30:08 +0530

    Initial Commit

--- modules/avatar-loader.js
+++ modules/avatar-loader.js
@@ -4,24 +4,20 @@
     const avatarIcon = document.querySelector('.hero-avatar .avatar-icon');
     if (!avatarImage) return;
 
-    if (window.innerWidth <= 968) {
-        avatarImage.style.display = 'none';
-        if (avatarIcon) avatarIcon.style.display = 'flex';
-        return;
-    }
-
-    avatarIcon.style.display = 'flex';
+    if (avatarIcon) avatarIcon.style.display = 'flex';
     avatarImage.style.display = 'none';
+    avatarImage.classList.remove('loaded');
 
     const img = new Image();
     img.onload = function() {
-        avatarIcon.style.display = 'none';
+        if (avatarIcon) avatarIcon.style.display = 'none';
         avatarImage.style.display = 'block';
         avatarImage.classList.add('loaded');
     };
     img.onerror = function() {
-        avatarIcon.style.display = 'flex';
+        if (avatarIcon) avatarIcon.style.display = 'flex';
         avatarImage.style.display = 'none';
+        avatarImage.classList.remove('loaded');
     };
     img.src = avatarImage.src;
 })();
